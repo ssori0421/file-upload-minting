60ea60dbb9f320e466ca5bbad055af3b7d99793f
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-17 09:30:07 +0900

    chore: 불필요한 파일 제거 및 기본 스타일 설정

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Flex } from '@chakra-ui/react';
+import { FC } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Flex
+      bgColor='red.100'
+      w='100%'
+      minH='100vh'
+      justifyContent='center'
+      alignItems='center'
+      flexDirection='column'
+    >
+      App
+    </Flex>
+  );
+};
 
-export default App
+export default App;
