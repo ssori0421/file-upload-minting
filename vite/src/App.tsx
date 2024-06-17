7af95a0bbed2bc8dcf7ebfecea48a807a8657fef
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-17 10:50:14 +0900

    chore: pinata에 이미지 업로드 하는 로직 분리해서 적용

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -21,14 +21,8 @@ const App: FC = () => {
     }
   };
 
-  const onChangeFile = async (e: ChangeEvent<HTMLInputElement>) => {
+  const uploadImage = async (formData: FormData) => {
     try {
-      if (!e.currentTarget.files) return;
-
-      const formData = new FormData();
-
-      formData.append('file', e.currentTarget.files[0]);
-
       const response = await axios.post(
         'https://api.pinata.cloud/pinning/pinFileToIPFS',
         formData,
@@ -41,7 +35,22 @@ const App: FC = () => {
         }
       );
 
-      console.log(response);
+      return `https://gray-obvious-prawn-797.mypinata.cloud/ipfs/${response.data.IpfsHash}`;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onChangeFile = async (e: ChangeEvent<HTMLInputElement>) => {
+    try {
+      if (!e.currentTarget.files) return;
+
+      const formData = new FormData();
+
+      formData.append('file', e.currentTarget.files[0]);
+
+      const imageUrl = await uploadImage(formData);
+      console.log(imageUrl);
     } catch (error) {
       console.error(error);
     }
