6d745d49aa5323f5f5652fe1cabcd7312749de94
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-17 09:39:06 +0900

    feat: ethers를 사용해서 지갑 연결하는 기능 추가

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,7 +1,28 @@
-import { Flex } from '@chakra-ui/react';
-import { FC } from 'react';
+import { Button, Flex } from '@chakra-ui/react';
+import { Contract, ethers } from 'ethers';
+import { JsonRpcSigner } from 'ethers';
+import { FC, useEffect, useState } from 'react';
 
 const App: FC = () => {
+  const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
+  const [contract, setContract] = useState<Contract | null>(null);
+
+  const onClickMetamask = async () => {
+    try {
+      if (!window.ethereum) return;
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      setSigner(await provider.getSigner());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(signer);
+  }, [signer]);
+
   return (
     <Flex
       bgColor='red.100'
@@ -11,7 +32,7 @@ const App: FC = () => {
       alignItems='center'
       flexDirection='column'
     >
-      App
+      <Button onClick={onClickMetamask}>지갑 연결하기</Button>
     </Flex>
   );
 };
